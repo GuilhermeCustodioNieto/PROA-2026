8ee59b4a7b0c2c6b59b0973dff4a602bbec74246
guilhermecustodionieto@gmail.com <guilhermecustodionieto@gmail.com>
2026-04-24 13:04:02 -0300

    feat: iniciado o estudo de reactjs

---
+++ react/projeto02/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  const nome = "Luana";
+  return (
+    <div>
+      <p>A {nome} é braço curto - little braço</p>
+    </div>
+  );
+}
+
+export default App;
